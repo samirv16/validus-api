a2be09c7d782171abff32172a2b31b7057e02043
samirv16 <samir.vallecillo@outlook.com>
2020-03-31 16:36:37 -0500

    creates database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_212744) do
+ActiveRecord::Schema.define(version: 2020_03_31_213504) do
 
   create_table "workouts", force: :cascade do |t|
+    t.string "name"
+    t.string "body"
+    t.integer "rounds"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
